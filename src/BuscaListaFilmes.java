c06d05bdd26070b6914233440d45472d0cd27d80
christianomj <christianomj@gmail.com>
2022-04-21 20:10:46 -0300

    Dia 2

--- src/BuscaListaFilmes.java
+++ src/BuscaListaFilmes.java
@@ -2,14 +2,50 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
 
 public class BuscaListaFilmes {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		System.out.println("Buscador de Filmes no IMDB");
 		
+		String json = buscarFilmes();
+		
+		
+		String listaFilmesJunto = json.substring(json.indexOf('[') + 1, json.indexOf(']'));
+		String [] listaFilmes = listaFilmesJunto.split("(?<=}),");
+	
+
+		
+		//System.out.println(listaFilmesJunto);
+		System.out.println(listaFilmes[0]);
+		System.out.println(listaFilmes[1]);
+		
+		
+		List<String> titles = new ArrayList<String>();
+		
+		for (int i = 0; i < listaFilmes.length; i++) {
+			titles.add(listaFilmes[i].substring(listaFilmes[i].indexOf("\"title\":") + 8, 
+					listaFilmes[i].indexOf(",", listaFilmes[i].indexOf("\"title\":"))));
+		}
+		
+		titles.forEach(System.out::println);
+		
+		List<String> images = new ArrayList<String>();
+		
+		for (int i = 0; i < listaFilmes.length; i++) {
+			images.add(listaFilmes[i].substring(listaFilmes[i].indexOf("\"image\":") + 9, 
+					listaFilmes[i].indexOf("\",", listaFilmes[i].indexOf("\"image\":"))));
+		}
+		
+		images.forEach(System.out::println);
+		
+	}
+
+	private static String buscarFilmes() throws IOException, InterruptedException {
 		// A chave da Api ficou na classe ApiKey que não está no github:
 		// Caso prefira, apague ou comente a linha do getApikey e faça uma atribuição direta:
 		// String apiKey = "suaApiKey";
@@ -24,9 +60,9 @@ public class BuscaListaFilmes {
 		
 		HttpClient client = HttpClient.newBuilder().build();
 		
-		HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+		// HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
 		
-		System.out.println(response.body());
+		return client.send(request, BodyHandlers.ofString()).body();
 	}
 
 }
